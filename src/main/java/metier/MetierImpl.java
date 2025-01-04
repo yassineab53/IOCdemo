9758d1b8870362de69a9499eb4187e444ee8df1b
yassine4real <yssnabbou2002@gmail.com>
2025-01-05 00:58:01 +0100

    a

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -7,10 +7,20 @@ import dao.IDao;
 
 public class MetierImpl implements IMetier{
     // couplage faible
-    private IDao dao = new DaoImplV2();
+    //depand des interfaces et pas de classe
+    //private IDao dao = new DaoImplV2();
+    private IDao dao;
     @Override
     public double calcul(){
         double t = dao.getData();
-        return t;
+        double res = t*23;
+        return res;
+    }
+
+    /*
+    Injecter dans la variable dao un objet d'une classe qui implemente l'interface IDao
+    */
+    public void setDao(IDao dao){
+        this.dao = dao;
     }
 }
