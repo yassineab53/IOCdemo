b5c1d85a2555a4d6b17dbe2c18f1110492e4a158
yassine4real <yssnabbou2002@gmail.com>
2025-01-05 01:33:20 +0100

    Injection des dépendances par instanciation statique

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -8,6 +8,9 @@ import metier.MetierImpl;
 
 public class PresentationV1 {
     public static void main(String[] args) {
+        /*
+        Injection des dépendances par instanciation statique
+         */
         DaoImpl d = new DaoImpl();
         MetierImpl metier=new MetierImpl();
         metier.setDao(d);
