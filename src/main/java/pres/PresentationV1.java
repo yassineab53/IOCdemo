92871d8dbde74571797b309168674a5a057175d6
yassine4real <yssnabbou2002@gmail.com>
2025-01-05 01:32:38 +0100

    a

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -8,7 +8,7 @@ import metier.MetierImpl;
 
 public class PresentationV1 {
     public static void main(String[] args) {
-        IDao d = new DaoImpl();
+        DaoImpl d = new DaoImpl();
         MetierImpl metier=new MetierImpl();
         metier.setDao(d);
         System.out.println("RES="+metier.calcul());
